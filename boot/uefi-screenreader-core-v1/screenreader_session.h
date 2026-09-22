dfcab688ed2c1cb52427f18526ff784b6fbdff97
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-22 17:44:30 +0200

    Add realtime chooser session API

--- boot/uefi-screenreader-core-v1/screenreader_session.h
+++ boot/uefi-screenreader-core-v1/screenreader_session.h
@@ -25,6 +25,14 @@ int sr_session_navigate(
     char first_letter
 );
 int sr_session_speak_hint(SrScreenReaderSession *session);
+int sr_session_repeat_focus(SrScreenReaderSession *session);
+int sr_session_chooser_open(SrScreenReaderSession *session);
+int sr_session_chooser_type(SrScreenReaderSession *session, char ch);
+int sr_session_chooser_backspace(SrScreenReaderSession *session);
+int sr_session_chooser_next(SrScreenReaderSession *session);
+int sr_session_chooser_previous(SrScreenReaderSession *session);
+int sr_session_chooser_select(SrScreenReaderSession *session);
+int sr_session_chooser_cancel(SrScreenReaderSession *session);
 const SrItem *sr_session_current(const SrScreenReaderSession *session);
 int sr_session_speech_complete(SrScreenReaderSession *session, SrSpeechEvent *next);
 
